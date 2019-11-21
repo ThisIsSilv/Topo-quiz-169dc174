6962c86b33467928783d6035f327d875462f5006
ThisIsSilv <54795019+ThisIsSilv@users.noreply.github.com>
2019-11-21 09:53:29 +0100

    Add files via upload

---
+++ topo.php
@@ -0,0 +1,108 @@
+<?php
+$i=0;
+//////////////////////////////////////////////////////////////////////
+echo "Wat is de hoofdstad van Japan".PHP_EOL;
+$tokyo = readline();
+if ($tokyo === "Tokyo"){
+    echo "Correct!".PHP_EOL;
+    $i++;
+}
+if ($tokyo !== "Tokyo") {
+    echo "Dat is niet correct, het is Tokyo".PHP_EOL;
+}
+//////////////////////////////////////////////////////////////////////
+echo "Wat is de hoofdstad van Mexico".PHP_EOL;
+$mexico = readline();
+if (mexico === "Mexico City"){
+    echo "Correct!".PHP_EOL;
+    $i++;
+}
+if (mexico !== "Mexico City") {
+    echo "Dat is niet correct, het is Mexico City".PHP_EOL;
+}
+//////////////////////////////////////////////////////////////////////
+echo "Wat is de hoofdstad van de USA".PHP_EOL;
+$usa = readline();
+if ($usa === "Washington DC"){
+    echo "Correct!".PHP_EOL;
+    $i++;
+}
+if ($usa !== "Washington DC") {
+    echo "Dat is niet correct, het is Washington DC".PHP_EOL;
+}
+//////////////////////////////////////////////////////////////////////
+echo "Wat is de hoofdstad van India".PHP_EOL;
+$india = readline();
+if (india === "New Delhi"){
+    echo "Correct!".PHP_EOL;
+    $i++;
+}
+if (india !== "New Delhi") {
+    echo "Dat is niet correct, het is New Delhi".PHP_EOL;
+}
+//////////////////////////////////////////////////////////////////////
+echo "Wat is de hoofdstad van Zuid-Korea".PHP_EOL;
+$korea = readline();
+if ($korea === "Seoul"){
+    echo "Correct!".PHP_EOL;
+    $i++;
+}
+if ($korea !== "Seoul") {
+    echo "Dat is niet correct, het is Seoul".PHP_EOL;
+}
+//////////////////////////////////////////////////////////////////////
+echo "Wat is de hoofdstad van China".PHP_EOL;
+$china = readline();
+if ($china === "Peking"){
+    echo "Correct!".PHP_EOL;
+    $i++;
+}
+if ($china !== "Peking") {
+    echo "Dat is niet correct, het is Peking".PHP_EOL;
+}
+//////////////////////////////////////////////////////////////////////
+echo "Wat is de hoofdstad van Nigeria".PHP_EOL;
+$nigeria = readline();
+if ($nigeria === "Abuja"){
+    echo "Correct!".PHP_EOL;
+    $i++;
+}
+if ($nigeria !== "Abuja") {
+    echo "Dat is niet correct, het is Abuja".PHP_EOL;
+}
+//////////////////////////////////////////////////////////////////////
+echo "Wat is de hoofdstad van Argentina".PHP_EOL;
+$argentina = readline();
+if ($argentina === "Buenos Aires"){
+    echo "Correct!".PHP_EOL;
+    $i++;
+}
+if ($argentina !== "Buenos Aires") {
+    echo "Dat is niet correct, het is Buenos Aires".PHP_EOL;
+}
+//////////////////////////////////////////////////////////////////////
+echo "Wat is de hoofdstad van Egypt".PHP_EOL;
+$egypt = readline();
+if ($egypt === "Cairo "){
+    echo "Correct!".PHP_EOL;
+    $i++;
+}
+if ($egypt !== "Cairo ") {
+    echo "Dat is niet correct, het is Cairo ".PHP_EOL;
+}
+//////////////////////////////////////////////////////////////////////
+echo "Wat is de hoofdstad van de UK".PHP_EOL;
+$uk = readline();
+if ($uk === "London "){
+    echo "Correct!".PHP_EOL;
+    $i++;
+}
+if ($uk !== "London ") {
+    echo "Dat is niet correct, het is London ".PHP_EOL;
+}
+echo "je hebt $i van de 10 goed geraden".PHP_EOL;
+
+
+
+
+
